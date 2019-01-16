279830bc71021c69e4f964ddc671b27858326fee
fherzt <franhdez97@hotmail.com>
2019-01-16 12:45:15 -0600

    no se que poner xD
    Co-Authored-By: Fran Hdez <fherzt@users.noreply.github.com>

--- Modulos/Encomiendas/encomiendas.php
+++ Modulos/Encomiendas/encomiendas.php
@@ -903,10 +903,10 @@ if($_SESSION['autenticado']!="yeah" || $t!=1){
                                   </thead>
                                   <tbody id="recargarListaEncomiendas">
                                     <?php
-                                    $consulta = pg_query($conexion, "SELECT * FROM pbencomienda");
+                                    $consulta = pg_query($conexion, "SELECT * FROM pbencomienda order by eid_encomienda asc");
 
                                     while($fila = pg_fetch_array($consulta)) {
-                                      $consulta_encomendero = pg_query($conexion, "SELECT * FROM paencomendero");
+                                      $consulta_encomendero = pg_query($conexion, "SELECT * FROM paencomendero WHERE eid_encomendero=$fila[3]");
                                       $encomendero = "";
 
                                       while ($fila_encomendero = pg_fetch_array($consulta_encomendero)) {

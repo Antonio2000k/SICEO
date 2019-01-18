5484390b3835c123355c4df0852e3132a367643e
alexorellana98 <42554127+alexorellana98@users.noreply.github.com>
2019-01-18 10:52:07 -0600

    reporte encomienda

--- plantillaEncomiendas.php
+++ plantillaEncomiendas.php
@@ -4,38 +4,120 @@
 	
 	class PDF extends FPDF
 	{
-		function Header()
-		{
-			$this->Image('../pdf/images/Siceo 2.0.png', 20, 20, 50 );
-			$this->SetFont('Arial','B',20);
-			$this->Ln(10);
+		function vcell($c_ancho,$c_alto,$x_posicion,$texto){ 
+			$altura=$c_alto/3; 
+
+			$primera=$altura+2; 
+
+			$segunda=$primera+$primera+3; 
+
+			$tercera=$segunda+13; 
+
+			$len=strlen($texto); 
+			if($len>=1){ 
+				$w_texto=str_split($texto,40); 
+				$this->SetX($x_posicion); 
+				$this->Cell($c_ancho,$primera,$w_texto[0],0,0,'C',0); 
+				$this->SetX($x_posicion); 
+				$this->Cell($c_ancho,$segunda,$w_texto[1],0,0,'C',0); 
+				$this->SetX($x_posicion); 
+				$this->Cell($c_ancho,$tercera,$w_texto[2],0,0,'C',0); 
+				
+				$this->SetX($x_posicion); 
+				$this->Cell($c_ancho,$c_alto,'',1,1,'C',0); 
+			}else{ 
+			    $this->SetX($x_posicion); 
+			    $this->Cell($c_ancho,$c_alto,$texto, 1,1,'C',0);
+			} 
+		} 
+
+		function vcellT($c_ancho,$c_alto,$x_posicion,$texto){ 
+			$altura=$c_alto/3; 
+
+			$primera=$altura+2; 
+
+			$segunda=$primera+$primera+3; 
+
+
+			$len=strlen($texto); 
+			if($len>=1){ 
+			$w_texto=str_split($texto,11); 
+				$this->SetX($x_posicion); 
+				$this->Cell($c_ancho,$primera,$w_texto[0],0,0,'C',0); 
+				$this->SetX($x_posicion); 
+				$this->Cell($c_ancho,$segunda,$w_texto[1],0,0,'C',0); 
+				$this->SetX($x_posicion); 
+				$this->Cell($c_ancho,$c_alto,'',0,0,'C',0); 
+			} else{ 
+			    $this->SetX($x_posicion); 
+			    $this->Cell($c_ancho,$c_alto,$texto,0,0,'C',0);
+			} 
+		}
+
+		function Header(){
+			$tipo=$_REQUEST['tipo'];
+			$tipos=$_REQUEST['tipos'];
+		    $year=$_REQUEST['year'];
+		    $rango=$_REQUEST['rango'];
+		    $mes=$_REQUEST['mes'];
+
+			$this->Image('images/Siceom.png', 35, 20, 40 );
+			$this->SetFont('times','B',14);
+			$this->Ln(4);
 			$this->Cell(30);
-			$this->Cell(220,10,utf8_decode("ÓPTICA"),0,1,'C');
+			$this->Cell(200,11,utf8_decode("ÓPTICA"),0,0,'C');
+			// 1º Datos del cliente
+			$this->Image('images/Grupo.png', 205, 20, 35 );
+			$this->SetXY(200, 23);
+			$this->SetFont('times','B',14);
 			$this->Cell(30);
-			$this->Cell(220,10,utf8_decode("VISIÓN CENTRAL"),0,1,'C');
+			$this->Cell(-180,5,utf8_decode("VISIÓN CENTRAL"),0,1,'C');
 			$this->Cell(30);
-			$this->SetFont('Arial','B',8);
-			$this->Cell(220,5,utf8_decode("TU VISIÓN, ES NUESTRA MISIÓN"),0,1,'C');
-			$this->Cell(30);
-			$this->Cell(220,10,'Av. Crescencio Miranda #2, 40mts al Sur del Cuartel, San Vicente.',0,1,'C');
-			$this->Cell(30);
-			$this->Cell(220,5,utf8_decode("Teléfono: 2328 - 9312"),0,1,'C');
+			$this->SetFont('times','B',8);
+			$this->Cell(200,5,utf8_decode(" \"TU VISIÓN, ES NUESTRA MISIÓN\""),0,1,'C');
+			$this->SetXY(40, 31);
+     		$this->Cell(200,5,'Av. Crescencio Miranda #2, 40mts al Sur del Cuartel, San Vicente.',0,1,'C');
+      		$this->SetXY(40, 34);
+			$this->Cell(195,5,utf8_decode("Teléfono: 2328 - 9312"),0,1,'C');
 			
-			$this->SetFont('Arial','B',16);
-			$this->Ln(5);
-			$this->Cell(30);
-			$this->Cell(220,10, 'Reporte de encomiendas',0,1,'C');
-			$this->Ln(5);
+			
+				$this->SetFont('times','B',16);
+				$this->Ln(5);
+				$this->Cell(30);
+				$this->Cell(200,10, 'Reporte de encomiendas ',0,1,'C');
+				$this->Ln(5);
+			
+			
+			
+
+			
+			$this->Ln(1);
+			$this->Cell(35); 
+
+
+			
+			
+
+			$this->SetDrawColor(0, 0, 0);
+			$this->SetLineWidth(0);
+
+			
 		}
 		
 		function Footer()
 		{
-			$this->SetY(-15);
-			$this->SetFont('Arial','I', 8);
-			$this->Cell(0,10, 'Pagina '.$this->PageNo().'/{nb}',0,0,'C' );
-			
-			$this->Cell(0,10,'Fecha y hora de imprecion: '.date('d/m/Y,h:i:s'),0,0,'R');
+			$fecha = new DateTime(null, new DateTimeZone('America/El_Salvador'));
+			$hora = $fecha->format("d/m/Y, h:i:s");
 
-		}		
+			$this->SetY(-30);
+			$this->SetFont('times','B',8);
+			$this->Cell(0,10, utf8_decode('Página '.$this->PageNo().'/{nb}'),0,0,'C' );
+			
+			$this->Cell(0,10,utf8_decode('Fecha y hora de impresión: '. $hora ),0,0,'R');
+
+		}
+		
+		
+			
 	}
 ?>
--- reporteEncomiendas.php
+++ reporteEncomiendas.php
@@ -8,34 +8,39 @@
 	//$query = "SELECT e.estado, m.id_municipio, m.municipio FROM t_municipio AS m INNER JOIN t_estado AS e ON m.id_estado=e.id_estado";
 	//$resultado = $mysqli->query($query);
 
-	$pdf = new PDF('L','mm','A4');
+	$pdf = new PDF('L','mm','letter');
 	$pdf->AliasNbPages();
 
 	$pdf->AddPage();
 
-
-	$pdf->SetFont('Arial','B',12);
-
-
 	$y = $pdf->GetY();
 	$pdf->SetY($y+5);
 
-	$pdf->SetX(15);
-	$pdf->SetFillColor(232,232,232);
-	$pdf->SetFont('Arial','B',8);
+	$pdf->SetX(8);
+	$pdf->SetFillColor(255, 255, 255);
+	$pdf->SetTextColor(0,0,0);
+	$pdf->SetFont('Arial','B',7);
 
-	$pdf->Cell(70,6,'NOMBRE',1,0,'C',1);
-	$pdf->Cell(20,6,'TELEFONO',1,0,'C',1);
-	$pdf->Cell(120,6,utf8_decode("FECHA DE ENVIO"),1,0,'C',1);
-	$pdf->Cell(30,6,utf8_decode("FECHA DE RECIBIDO"),1,0,'C',1);
-	$pdf->Cell(30,6,'DETALLE',1,1,'C',1);
+	$pdf->Cell(60,6,'ENCOMENDERO',0,0,'C',1);
+	$pdf->Cell(18,6,'TELEFONO',0,0,'C',1);
+	$pdf->Cell(133,6,utf8_decode("DETALLE"),0,0,'C',1);
+	$pdf->Cell(25,6,utf8_decode("FECHA DE ENVIO"),0,0,'C',1);
+	$pdf->Cell(28,6,'FECHA DE RECIBIDO',0,1,'C',1);		
+	$pdf->SetDrawColor(25, 25, 112);
+	$pdf->SetLineWidth(2);
+	$pdf->Line(8, $pdf->GetY()+1 , 271 , $pdf->GetY()+1);
+
+	$y = $pdf->GetY();
+	$pdf->SetY($y+3);
+	
 
 	$query_s= pg_query($conexion, "SELECT * FROM pbencomienda WHERE eid_encomienda = $id");
 
-	$pdf->SetFont('Arial','B',9);
-	$pdf->SetX(15);
+	$pdf->SetFont('Arial','B',8);
+	$pdf->SetX(8);
 	$pdf->SetFillColor(255,255,255);
-
+	$pdf->SetDrawColor(0, 0, 0);
+	$pdf->SetLineWidth(0);
 	while($fila = pg_fetch_array($query_s)) {
 		// $pdf->Cell(70,8,'Alexander Enmanuel Orellana Corvera',1,0,'C',1);
 		// $pdf->Cell(20,8,'M',1,0,'C',1);
@@ -53,12 +58,48 @@
 			$telefono = $fila_encomendero[3];
 		}
 
-		$pdf->Cell(70,8,$nombre,1,0,'C',1);
-		$pdf->Cell(20,8,$telefono,1,0,'C',1);
-		$pdf->Cell(120,8,date('d/m/Y', strtotime($fila[1])),1,0,'C',1);
-		$pdf->Cell(30,8,date('d/m/Y', strtotime($fila[5])),1,0,'C',1);
-		$pdf->Cell(30,8,$fila[4],1,1,'C',1);
+		$pdf->Cell(60,8,$nombre,1,0,'C',1);
+		$pdf->Cell(18,8,$telefono,1,0,'C',1);
+		$pdf->Cell(133,8,$fila[4],1,0,'C',1);
+		$pdf->Cell(25,8,date('d/m/Y', strtotime($fila[1])),1,0,'C',1);
+		if($fila[2]== 't'){
+			$pdf->Cell(28,8,date('d/m/Y', strtotime($fila[5])),1,1,'C',1);
+		}else if($fila[2]== 'f'){
+			$pdf->Cell(28,8,'PENDIENTE',1,1,'C',1);
+		}
 	}
+	$query_detalle= pg_query($conexion, "SELECT pcdetalle_encomienda.cmodelo,  pcdetalle_encomienda.cmaterial, pcdetalle_encomienda.ctipo, pbencomienda.eid_encomienda, paclientes.cnombre, pcdetalle_encomienda.eid_encomienda, paclientes.capellido , pcdetalle_encomienda.eid_encomienda 
+			FROM pcdetalle_encomienda INNER JOIN pbencomienda ON pcdetalle_encomienda.eid_encomienda = pbencomienda.eid_encomienda INNER JOIN paclientes ON pcdetalle_encomienda.cid_cliente = paclientes.eid_cliente WHERE pcdetalle_encomienda.eid_encomienda = $id");
+	
+
+	$pdf->Ln(10);
+	$pdf->SetX(8);
+	$pdf->SetFont('times','B',13);
+	$pdf->Cell(269,6,'Detalle de paquete enviado',0,1,'C',1);
+	$pdf->Ln(2);
+	$pdf->SetX(50);
+	$pdf->SetFont('arial','B',8);
+	$pdf->Cell(60,8,'CLIENTE',0,0,'C',1);
+	$pdf->Cell(60,8,'MODELO',0,0,'C',1);
+	$pdf->Cell(30,8,'MATERIAL',0,0,'C',1);
+	$pdf->Cell(30,8,'TIPO',0,1,'C',1);
+	
+	$pdf->SetDrawColor(25, 25, 112);
+	$pdf->SetLineWidth(2);
+	$pdf->Line(50, $pdf->GetY() , 230 , $pdf->GetY());
+	$pdf->Ln(2);
+	$pdf->SetFillColor(255,255,255);
+	$pdf->SetDrawColor(0, 0, 0);
+	$pdf->SetLineWidth(0);
+
+		while($fila_d = pg_fetch_array($query_detalle)) {
+			$pdf->SetX(50);
+			$pdf->Cell(60,8,$fila_d[4] . ' ' . $fila_d[6],1,0,'C',1);
+			$pdf->Cell(60,8,$fila_d[0],1,0,'C',1);
+			$pdf->Cell(30,8,$fila_d[1],1,0,'C',1);
+			$pdf->Cell(30,8,$fila_d[2],1,1,'C',1);
+			
+		}
 
 	$pdf->Output();
 
